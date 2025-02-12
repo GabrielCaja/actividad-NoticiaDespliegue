409e27f53cad23c22b5d005c04e9946434eed443
VictorPotenciano <victor.potenciano@live.u-tad.com>
2025-02-12 20:36:11 +0100

    Cambios finales

--- api/index.php
+++ api/index.php
@@ -145,3 +145,78 @@
 
 </body>
 </html>
+
+
+<?php
+    require_once "RSSElPais.php";
+    require_once "conexionBBDD.php";
+
+    function filtros($sql, $link)
+    {
+        $result = pg_query($link, $sql);
+
+        if (! $result) {
+            echo "Error en la consulta SQL: " . pg_last_error($link);
+            return;
+        }
+
+        while ($arrayFiltro = pg_fetch_assoc($result)) {
+            echo "<tr>";
+            echo "<td style='border: 1px solid #E4CCE8; padding: 8px; max-width: 20%; overflow: hidden; text-overflow: ellipsis;'>" . $arrayFiltro['titulo'] . "</td>";
+            echo "<td style='border: 1px solid #E4CCE8; padding: 8px; max-width: 40%; overflow: hidden; text-overflow: ellipsis;'>" . $arrayFiltro['contenido'] . "</td>";
+            echo "<td style='border: 1px solid #E4CCE8; padding: 8px; max-width: 20%; overflow: hidden; text-overflow: ellipsis;'>" . $arrayFiltro['descripcion'] . "</td>";
+            echo "<td style='border: 1px solid #E4CCE8; padding: 8px; max-width: 10%; overflow: hidden; text-overflow: ellipsis;'>" . $arrayFiltro['categoria'] . "</td>";
+            echo "<td style='border: 1px solid #E4CCE8; padding: 8px; max-width: 10%; overflow: hidden; text-overflow: ellipsis;'><a href='" . $arrayFiltro['link'] . "' target='_blank'>" . $arrayFiltro['link'] . "</a></td>";
+            $fecha           = date_create($arrayFiltro['fpubli']);
+            $fechaConversion = date_format($fecha, 'd-M-Y');
+            echo "<td style='border: 1px solid #E4CCE8; padding: 8px;'>" . $fechaConversion . "</td>";
+            echo "</tr>";
+        }
+
+        // Cerrar la tabla HTML
+        echo "</table>";
+    }
+
+    // Verifica si la conexión falló
+    if (! $link) {
+        die("Conexión fallida: " . pg_last_error());
+    } else {
+        echo "<table style='border: 5px #E4CCE8 solid;'>";
+        echo "<tr><th><p style='color: #66E9D9;'>TITULO</p ></th><th><p style='color: #66E9D9;'>CONTENIDO</p ></th><th><p style='color: #66E9D9;'>DESCRIPCIÓN</p ></th><th><p style='color: #66E9D9;'>CATEGORÍA</p ></th><th><p style='color: #66E9D9;'>ENLACE</p ></th><th><p style='color: #66E9D9;'>FECHA DE PUBLICACIÓN</p ></th></tr><br>";
+
+        if (isset($_GET['filtrar'])) {
+            $cat     = isset($_GET["categoria"]) ? $_GET["categoria"] : '';
+            $fech    = isset($_GET["fecha"]) ? date("Y-m-d", strtotime($_GET["fecha"])) : '';
+            $palabra = isset($_GET["buscar"]) ? $_GET["buscar"] : '';
+
+            $sql        = "SELECT * FROM elpais";
+            $conditions = [];
+
+            if ($cat != "") {
+                $conditions[] = "categoria ILIKE '%$cat%'";
+            }
+            if ($fech != '' && $fech != '1970-01-01') {
+                $conditions[] = "fpubli = '$fech'";
+            }
+            if (! empty($palabra)) {
+                $conditions[] = "descripcion ILIKE '%$palabra%'";
+            }
+
+            if (! empty($conditions)) {
+                $sql .= " WHERE " . implode(' AND ', $conditions);
+            }
+
+            $sql .= " ORDER BY fpubli DESC";
+
+            filtros($sql, $link);
+        } else {
+            $sql = "SELECT * FROM elpais ORDER BY fpubli DESC LIMIT 20";
+            filtros($sql, $link);
+        }
+
+        echo "</table>";
+    }
+
+    // Cerrar la conexión a la base de datos
+    pg_close($link);
+?>
